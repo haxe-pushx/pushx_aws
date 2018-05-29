27e90a8747b9bbb141aea04ddb5ae372a9cb775c
Kevin Leung <kevinresol@gmail.com>
2018-05-29 21:39:31 +0800

    Add some stuff

--- src/pushx/aws/iot/NodeAwsIotPusher.hx
+++ src/pushx/aws/iot/NodeAwsIotPusher.hx
@@ -1,17 +1,31 @@
 package pushx.aws.iot;
 
+import js.aws.iotdata.IotData;
+
 using tink.CoreApi;
+
+private typedef Options<Data:{}> = {
+	?iot:{},
+	?idToTopic:String->String, 
+	?jsonify:Payload<Data>->String,
+}
 
 class NodeAwsIotPusher<Data:{}> implements pushx.Pusher<Data> {
 	
 	var iotData:IotData;
-	var idToTopic:String->String;
-	var jsonify:Payload<Data>->String;
 	
-	public function new(config, ?options:{?idToTopic:String->String, ?jsonify:Payload<Data>->String}) {
-		iotData = new IotData(config);
-		idToTopic = options != null && options.idToTopic != null ? options.idToTopic : function(id) return 'push/$id';
-		jsonify = options != null && options.jsonify != null ? options.jsonify : haxe.Json.stringify.bind(_, null, null);
+	public function new(options:Options<Data>) {
+		iotData = new IotData(options.iot);
+		if(options.idToTopic != null) idToTopic = options.idToTopic;
+		if(options.jsonify != null) jsonify = options.jsonify;
+	}
+	
+	dynamic function idToTopic(id:String) {
+		return 'push/$id';
+	}
+	
+	dynamic function jsonify(payload:Payload<Data>) {
+		return haxe.Json.stringify(payload);
 	}
 	
 	public function single(id:String, payload:pushx.Payload<Data>):Promise<pushx.Result> {
@@ -35,10 +49,4 @@ class NodeAwsIotPusher<Data:{}> implements pushx.Pusher<Data> {
 			}, function(err, _) cb(err == null ? Success({errors: []}) : Failure(Error.ofJsError(err)))));
 			
 	}
-}
-
-@:jsRequire('aws-sdk', 'IotData')
-private extern class IotData {
-	function new(config:{endpoint:String}):Void;
-	function publish(params:{topic:String, payload:String, qos:Int}, cb:js.Error->Dynamic->Void):Void;
 }
---
+++ src/pushx/aws/sns/NodeAwsSnsPusher.hx
@@ -0,0 +1,75 @@
+package pushx.aws.sns;
+
+import js.aws.sns.SNS;
+
+using tink.CoreApi;
+
+private typedef Options<Data:{}> = {
+	application:String,
+	?sns:{},
+	?toMessage:Payload<Data>->String,
+}
+
+@:build(futurize.Futurize.build())
+class NodeAwsSnsPusher<Data:{}> implements pushx.Pusher<Data> {
+	
+	var sns:SNS;
+	var application:String;
+	
+	public function new(options:Options<Data>) {
+		sns = new SNS(options.sns);
+		application = options.application;
+		if(options.toMessage != null) toMessage = options.toMessage;
+	}
+	
+	dynamic function toMessage(payload:Payload<Data>)
+		return switch payload.notification {
+			case null | {body: null}: '<empty>';
+			case n: n.body;
+		}
+	
+	public function single(id:String, payload:pushx.Payload<Data>):Promise<pushx.Result> {
+		return createEndpoint(id)
+			.next(function(arn) {
+				return Future.async(
+					function(cb) sns.publish(
+						{
+							TargetArn: arn,
+							Message: toMessage(payload),
+							MessageStructure: 'json',
+						},
+						function(err, _) cb(err == null ? Success({errors: []}) : Failure(Error.ofJsError(err)))
+					)
+				);
+			});
+	}
+	
+	public function multiple(ids:Array<String>, payload:pushx.Payload<Data>):Promise<pushx.Result> {
+		return Promise.inParallel([for(id in ids) single(id, payload)])
+			.next(function(results) {
+				var errors = [];
+				for(result in results) errors = errors.concat(result.errors);
+				return {errors: errors};
+			});
+	}
+	
+	public function topic(topic:String, payload:pushx.Payload<Data>):Promise<pushx.Result> {
+		return Future.async(function(cb) sns.publish({
+				TopicArn: topic,
+				Message: toMessage(payload),
+			}, function(err, _) cb(err == null ? Success({errors: []}) : Failure(Error.ofJsError(err)))));
+			
+	}
+	
+	function createEndpoint(token:String) {
+		return @:futurize sns.createPlatformEndpoint({
+			PlatformApplicationArn: application,
+			Token: token,
+			Attributes: {
+				Enabled: 'true',
+				Token: token,
+				CustomUserData: '',
+			}
+		}, $cb1).next(function(o) return o.EndpointArn);
+	}
+}
